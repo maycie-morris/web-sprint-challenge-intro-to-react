37e4d083e409005859055836389d6e5912f3db9a
Maycie Morris <maycie@MacBook-Pro.local>
2020-08-14 08:28:14 -0700

    Started character card

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,20 @@
 // Write your Character component here
+
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard'
+
+
+function Character() {
+
+    const [character, setCharacter] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/character/").then((response) => {
+            setCharacter(response.data)
+        });
+    }, []);
+}
+
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const CharacterCard = props => {
+    return (
+        <li className="character-card" key={props.id}>
+            <img src= {props.image} />
+            <h2>Name: {props.name}</h2>
+            <p>Status: {props.status} </p>
+            <p>Species: {props.species} </p>
+            <p>Gender: {props.gender} </p>
+            <p>Origin: {props.origin.name} </p>
+        </li>
+    )
+}
